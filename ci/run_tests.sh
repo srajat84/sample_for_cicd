903b2320a3f784746f1aa271fb7f97b8abc79f0a
Rajat Singhal <rajasinghal@gmail.com>
2020-09-09 22:21:58 +0530

    feat(cicd): change job name

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -5,4 +5,4 @@ echo 'Running tests'
 python -m unittest discover -s tests/ -p "test*.py" -v
 
 mkdir reports
-echo 'test report' >> reports/fake_test_report.html
+echo '<p><b>this is a test report</b></p>' >> reports/fake_test_report.html
